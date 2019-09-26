1b7704a92efaf267a34ce03c67d94ff96052cef3
nhacpham34 <55837053+nhacpham34@users.noreply.github.com>
2019-09-26 22:38:43 +0700

    Add files via upload

---
+++ BTVN/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace BTVN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.slidePic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slidePic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(-1, 391);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(736, 108);
+            this.listBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::BTVN.Properties.Resources._7;
+            this.pictureBox7.Location = new System.Drawing.Point(638, 388);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(97, 62);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 2;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::BTVN.Properties.Resources._6;
+            this.pictureBox6.Location = new System.Drawing.Point(541, 388);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(97, 62);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 2;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::BTVN.Properties.Resources._5;
+            this.pictureBox5.Location = new System.Drawing.Point(447, 388);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(97, 62);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::BTVN.Properties.Resources._4;
+            this.pictureBox4.Location = new System.Drawing.Point(286, 365);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(155, 85);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::BTVN.Properties.Resources._3;
+            this.pictureBox3.Location = new System.Drawing.Point(183, 388);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(97, 62);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::BTVN.Properties.Resources._2;
+            this.pictureBox2.Location = new System.Drawing.Point(92, 388);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(97, 62);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BTVN.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 388);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // slidePic
+            // 
+            this.slidePic.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.slidePic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.slidePic.Image = global::BTVN.Properties.Resources._4;
+            this.slidePic.Location = new System.Drawing.Point(-1, 0);
+            this.slidePic.Name = "slidePic";
+            this.slidePic.Size = new System.Drawing.Size(736, 390);
+            this.slidePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slidePic.TabIndex = 4;
+            this.slidePic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 446);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.slidePic);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Uzumaki Naruto";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slidePic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox slidePic;
+    }
+}
+
---
+++ BTVN/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTVN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int imgNumber = 1, img1, img2, img3, img4, img5, img6, img7;
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            LoadNextImage();
+        }
+
+        private void LoadNextImage()
+        {
+            if (imgNumber == 10)
+            {
+                imgNumber = 1;
+            }
+            slidePic.ImageLocation = string.Format(@"Images\{0}.jpg", imgNumber);
+
+            if (imgNumber == 1) img1 = 8;
+            else if (imgNumber == 2) img1 = 9;
+            else if (imgNumber == 3) img1 = 10;
+            else img1 = imgNumber - 3;
+            pictureBox1.ImageLocation = string.Format(@"Images\{0}.jpg", img1);
+
+            if (imgNumber == 1) img2 = 9;
+            else if (imgNumber == 2) img2 = 10;
+            else img2 = imgNumber - 2;
+            pictureBox2.ImageLocation = string.Format(@"Images\{0}.jpg", img2);
+
+            if (imgNumber == 1) img3 = 10;
+            else img3 = imgNumber - 1;
+            pictureBox3.ImageLocation = string.Format(@"Images\{0}.jpg", img3);
+
+            img4 = imgNumber;
+            pictureBox4.ImageLocation = string.Format(@"Images\{0}.jpg", img4);
+
+            if (imgNumber == 10) img5 = 1;
+            else img5 = imgNumber + 1;
+            pictureBox5.ImageLocation = string.Format(@"Images\{0}.jpg", img5);
+
+            if (imgNumber == 10) img6 = 2;
+            else if (imgNumber == 9) img6 = 1;
+            else img6 = imgNumber + 2;
+            pictureBox6.ImageLocation = string.Format(@"Images\{0}.jpg", img6);
+
+            if (imgNumber == 10) img7 = 3;
+            else if (imgNumber == 9) img7 = 2;
+            else if (imgNumber == 8) img7 = 1;
+            else img7 = imgNumber + 3;
+            pictureBox7.ImageLocation = string.Format(@"Images\{0}.jpg", img7);
+
+            imgNumber++;
+
+        }
+    }
+}
